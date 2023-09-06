4f4101eba1937a1de34cda5400242fc231fb6a0e
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2023-09-06 15:56:51 +0100

    added missing exports

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ export { default as Highlight } from "./layout/Highlight/Highlight.svelte";
 export { default as List } from "./layout/List/List.svelte";
 export { default as NavSection } from "./layout/NavSections/NavSection.svelte";
 export { default as NavSections } from "./layout/NavSections/NavSections.svelte";
+export { default as Notice } from "./layout/Notice/Notice.svelte";
+export { default as PhaseBanner } from "./layout/NavSections/PhaseBanner.svelte";
 export { default as Scroller } from "./layout/Scroller/Scroller.svelte";
 export { default as ScrollerSection } from "./layout/Scroller/ScrollerSection.svelte";
 export { default as Section } from "./layout/Section/Section.svelte";
