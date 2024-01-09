1535ce65ca64c263bf1ae0090b2742497cc7bacd
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2024-01-09 09:27:45 +0000

    new npm version

---
+++ docs/assets/quickstart.stories-4908a790.js
@@ -0,0 +1,24 @@
+import{M as i}from"./chunk-PCJTTTQV-d7a63c60.js";import{p as s,j as e}from"./jsx-runtime-539b76a3.js";import{u as a}from"./index-297ebefe.js";import"./iframe-1ee7b173.js";import"../sb-preview/runtime.js";import"./extends-b40c5bac.js";import"./_commonjsHelpers-725317a4.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-1a79a776.js";import"./index-356e4a49.js";function o(n){const r=Object.assign({h1:"h1",p:"p",a:"a",h2:"h2",h3:"h3",pre:"pre",code:"code"},a(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Contributing/Quickstart",parameters:{...s}}),`
+`,e.jsx(r.h1,{id:"needs-updating",children:"!!!NEEDS UPDATING!!!"}),`
+`,e.jsx(r.h1,{id:"contributing",children:"Contributing"}),`
+`,e.jsx(r.p,{children:"Reuters Graphics team members can contribute new components, and we welcome pull requests for any of our existing components from the community."}),`
+`,e.jsxs(r.p,{children:["Components are documented (and tested) using ",e.jsx(r.a,{href:"https://storybook.js.org",target:"_blank",rel:"nofollow noopener noreferrer",children:"Storybook"}),"."]}),`
+`,e.jsx(r.h2,{id:"adding-a-new-chart-component",children:"Adding a new chart component"}),`
+`,e.jsx(r.h3,{id:"prereqs",children:"Prereqs"}),`
+`,e.jsxs(r.p,{children:["Before you start, it's probably a good idea to read up a bit on ",e.jsx(r.a,{href:"https://storybook.js.org/docs/svelte/get-started/introduction",target:"_blank",rel:"nofollow noopener noreferrer",children:"Storybook"}),", the framework used to document components."]}),`
+`,e.jsx(r.h3,{id:"quickstart",children:"Quickstart"}),`
+`,e.jsx(r.p,{children:"Make a new git branch for your new component, like..."}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{children:`git checkout -b my-component
+`})}),`
+`,e.jsx(r.p,{children:"This library includes a basic template for creating and documenting your component with Storybook you can use to get started."}),`
+`,e.jsx(r.p,{children:"Just run..."}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{children:`yarn new
+`})}),`
+`,e.jsx(r.p,{children:"... which will create a new directory for your component and copy over an example Svelte component and story page."}),`
+`,e.jsx(r.p,{children:"To start developing your component, start the dev server with..."}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{children:`yarn start
+`})}),`
+`,e.jsx(r.p,{children:"When you're ready to share your chart, commit your branch to GitHub, make a PR and we'll get it published!"}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{children:`git push origin my-chart
+`})})]})}function c(n={}){const{wrapper:r}=Object.assign({},a(),n.components);return r?e.jsx(r,{...n,children:e.jsx(o,{...n})}):o(n)}const d=()=>{throw new Error("Docs-only story")};d.parameters={docsOnly:!0};const t={title:"Contributing/Quickstart",parameters:{...s},tags:["stories-mdx"],includeStories:["__page"]};t.parameters=t.parameters||{};t.parameters.docs={...t.parameters.docs||{},page:c};const w=["__page"];export{w as __namedExportsOrder,d as __page,t as default};
+//# sourceMappingURL=quickstart.stories-4908a790.js.map
