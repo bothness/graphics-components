e2e19d78f457324de0ba6f6cf90e68a23d4ab9e7
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2024-03-20 13:57:29 +0000

    fix accordion styling for design system breaking change

--- src/layout/Accordion/AccordionItem.svelte
+++ src/layout/Accordion/AccordionItem.svelte
@@ -23,12 +23,11 @@
   const elements = getContext("elements");
   const allOpen = getContext("allOpen");
 
-  function updateAllOpen(open) {
+  function updateAllOpen(e) {
     if (el && $elements) {
-      $allOpen = open ? [...$elements].every((el) => el.open) : false;
+      $allOpen = e.newState === "open" ? [...$elements].every((el) => el.open) : false;
     }
   }
-  $: updateAllOpen(open);
 
   onMount(() => {
     $elements.add(el);
@@ -38,25 +37,25 @@
 
 <details
   id="accordion-1"
-  class="ons-details ons-js-details ons-details--accordion"
+  class="ons-collapsible ons-js-collapsible ons-details--accordion"
   data-group="accordion"
   bind:this="{el}"
   bind:open="{open}"
+  on:toggle="{updateAllOpen}"
 >
-  <summary class="ons-details__heading ons-js-details-heading">
-    <h2 class="ons-details__title">{title}</h2>
-    <span class="ons-details__icon">
+  <summary class="ons-collapsible__heading ons-js-collapsible-heading ons-details__heading">
+    <h2 class="ons-collapsible__title">{title}</h2>
+    <span class="ons-collapsible__icon">
       <svg
-        class="ons-svg-icon"
+        class="ons-icon"
         viewBox="0 0 8 13"
         xmlns="http://www.w3.org/2000/svg"
         focusable="false"
         fill="currentColor"
-      >
-        <path
+        ><path
           d="M5.74,14.28l-.57-.56a.5.5,0,0,1,0-.71h0l5-5-5-5a.5.5,0,0,1,0-.71h0l.57-.56a.5.5,0,0,1,.71,0h0l5.93,5.93a.5.5,0,0,1,0,.7L6.45,14.28a.5.5,0,0,1-.71,0Z"
-          transform="translate(-5.02 -1.59)"></path>
-      </svg>
+          transform="translate(-5.02 -1.59)"></path></svg
+      >
     </span>
   </summary>
   <div id="{id}-content" class="ons-details__content ons-js-details-content">
@@ -77,5 +76,11 @@
   }
   .ons-details__heading:hover:not(:focus) .ons-details__title {
     text-decoration: underline solid var(--link-hover, --ons-color-text-link-hover) 2px;
+  }
+  .ons-collapsible .ons-collapsible__icon {
+    top: 0.8rem;
+  }
+  .ons-collapsible[open] .ons-collapsible__icon {
+    top: 1.2rem;
   }
 </style>
