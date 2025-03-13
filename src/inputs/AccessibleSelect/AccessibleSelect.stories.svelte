544c7bf77f531fd089bcc0752cb97c1422726d67
Henry Lau <2945099+henryjameslau@users.noreply.github.com>
2025-03-13 11:12:49 +0000

    starting accessibleselect

---
+++ src/inputs/AccessibleSelect/AccessibleSelect.stories.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+
+  // @ts-ignore
+  import componentDocs from "./docs/component.md?raw";
+  import AccessibleSelect from "./AccessibleSelect.svelte";
+  import { withComponentDocs } from "../../js/withParams.js";
+  import options from "./options.js";
+
+  let searchValue;
+</script>
+
+<Meta
+  title="Inputs/AccessibleSelect"
+  component="{AccessibleSelect}"
+  argTypes="{{}}"
+  {...withComponentDocs(componentDocs)}
+/>
+
+<Template let:args>
+  <AccessibleSelect
+    id="search"
+    label="Find an option"
+    mode="search"
+    groupKey="group"
+    options="{options}"
+    bind:value="{searchValue}"
+  />
+</Template>
+
+<Story name="Search mode" args="{{ id: 'search', label: 'Find an option' }}" />
+
+<Story name="Dropdown mode" args="{{ id: 'dropdown', label: 'Select this' }}" />
