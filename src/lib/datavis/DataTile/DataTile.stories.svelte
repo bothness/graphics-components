348d08511dcef476a917c897c21f094c05dec0c4
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-06-18 10:08:23 +0100

    add DataTile component

---
+++ src/lib/datavis/DataTile/DataTile.stories.svelte
@@ -0,0 +1,28 @@
+<script module>
+	import { defineMeta } from "@storybook/addon-svelte-csf";
+	import { withComponentDocs } from "../../js/withParams.js";
+	import DataTile from "./DataTile.svelte";
+	import componentDocs from "./docs/component.md?raw";
+
+	const { Story } = defineMeta({
+		title: "Data visualisation/DataTile",
+		component: DataTile,
+		tags: ["autodocs"],
+		argTypes: {
+			mode: {
+				control: { type: "select" }
+			}
+		},
+		parameters: withComponentDocs(componentDocs)
+	});
+</script>
+
+<Story
+	name="Big number"
+	args={{
+		title: "Consumer Price Index (CPI)",
+		value: "up 2.5%",
+		caption: "in 12 months to December 2024",
+		source: "Source: ONS"
+	}}
+/>
---
+++ src/lib/datavis/DataTile/DataTile.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Card from "$lib/layout/Card/Card.svelte";
+
+	/**
+	 * Title of card
+	 * @type {string}
+	 */
+	export let title = "";
+	/**
+	 * Optional: URL link for card title
+	 * @type {string}
+	 */
+	export let href = "";
+	/**
+	 * Big number (expects a pre-formatted text string)
+	 * @type {string|number}
+	 */
+	export let value = "";
+	/**
+	 * Mode for the data tile
+	 * @type {"number"|"sparkline"|"bar"}
+	 */
+	export let mode = "number";
+	/**
+	 * Text caption describing the number
+	 * @type {string}
+	 */
+	export let caption = "";
+	/**
+	 * An array of data (required for charts)
+	 * @type {object[]|null}
+	 */
+	export let data = null;
+	/**
+	 * Text describing the source of the data
+	 * @type {string}
+	 */
+	export let source = "";
+</script>
+
+<Card {title} {href} mode="featured">
+	{#if value}
+		<p class="headline-figures__figure ons-u-fs-3xl ons-u-fs-2xl@m">{value}</p>
+	{/if}
+	{#if caption}
+		<p class="headline-figures__supporting-text ons-u-fs-r">{@html caption}</p>
+	{/if}
+	{#if source}
+		<p class="ons-u-fs-s">{source}</p>
+	{/if}
+</Card>
